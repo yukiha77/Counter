9f125710f9aca47f7e3e50bd2cca9451eed3ba57
Liz <Liz@Liz-3.local>
2015-04-26 15:23:07 +0900

    基底

--- Counter/Assets/Scripts/Base/CharacterBase.cs
+++ Counter/Assets/Scripts/Base/CharacterBase.cs
@@ -9,7 +9,21 @@ public class CharacterBase : MonoBehaviour {
 	[SerializeField]
 	protected int speed;
 	// 生死
-	protected bool life = true;
+	protected bool lifeFlag = true;
+	// タイマー
+	protected float timer;
+	// ショット
+	[HideInInspector]
+	public bool shotFlag;
+	// セカンダリショット
+	[HideInInspector]
+	public bool subShotFlag;
+	// ブースト
+	[HideInInspector]
+	public bool boostFlag;
+	// 必殺技(ボム)
+	[HideInInspector]
+	public bool exWeaponFlag;
 
 	// 移動 1.左右 2.上下 3.減速
 	protected void Move(float ix, float iz) {
@@ -17,13 +31,50 @@ public class CharacterBase : MonoBehaviour {
 		GetComponent<Rigidbody>().velocity = direction * speed;
 	}
 
-	protected void Weapon(bool ishot, bool iblade) {
+	// 発射入力
+	protected void Shot(bool ishot, bool isubShot) {
+		shotFlag 	= ishot;
+		subShotFlag = isubShot;
 	}
 
-	protected void IsDead(int shield) {
-		if (shield < 0)
-			life = false;
+	// ブースト入力
+	protected void Boost(bool iboost) {
+		boostFlag = iboost;
 	}
 
-	public bool GetLife(bool ilife) {return life;}
+	// 必殺技入力
+	protected void ExWeapon(bool iexWeapon) {
+		exWeaponFlag = iexWeapon;
+	}
+
+	// 生死判定   1.シールド
+	protected void IsDead(int ishield) {
+		if (ishield < 0)
+			Dead();
+
+		if (lifeFlag != false)
+			return;
+
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		Destroy(player);
+	}
+
+	protected void Timer() { timer += Time.deltaTime; }
+
+	// 生死フラグ
+	protected void Dead() { lifeFlag = false; }
+	protected void Revival() { lifeFlag = true; }
+
+	// ブーストフラグ
+	protected void OnBoost() { boostFlag = true; }
+	protected void OffBoost() { boostFlag = false; }
+
+	// ダメージ処理
+	public void SetShieldDamage(int attack) { shield -= attack; }
+
+	// フラグ取得
+	public bool GetIsLife(bool ilife) { return lifeFlag; }
+	public bool GetIsShot(bool ishot) { return shotFlag; }
+	public bool GetIsSubShot(bool ishot) { return subShotFlag; }
+	public bool GetIsBoost(bool iboost) { return boostFlag; }
 }
---
+++ Counter/Assets/Scripts/Base/WeaponBase.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponBase : MonoBehaviour {
+	// 威力
+	[SerializeField]
+	protected int damage;
+	// 速度
+	[SerializeField]
+	protected int speed;
+	// レベル
+	protected int level;
+	// 経験値
+	protected int exp;
+	// 使用する装備
+	[SerializeField]
+	protected int useWeapon;
+	// ショットのパターン
+	[SerializeField]
+	protected int pattern;
+	// Wayの数
+	protected int way;
+	// ファイアレート
+	[SerializeField]
+	protected float waitTimer;
+	// 装備使用機体
+	protected GameObject user;
+	// 弾の種類
+	protected GameObject[] weaponType;
+	// タイマー
+	private float timer;
+
+	// 装備の使用   1.ショットフラグ
+	protected void Shot(bool ishot) {
+		if (!ishot)
+			return;
+
+		Timer();
+		if (timer > waitTimer) {
+			WeaponGenerator(user, useWeapon);
+			timer = 0;
+		}
+	}
+
+	// それぞれの装備の挙動	   1.移動パターン
+	protected void Move(int pattern) {
+		switch (pattern) {
+		case 1:
+			WayShot(way);
+			break;
+		}
+	}
+
+	// ウェイショット   1.way数
+	void WayShot(int way) {
+		Vector3 direction;
+		switch (way) {
+		case 1:
+			direction = transform.forward;
+			GetComponent<Rigidbody>().velocity = direction * speed;
+			break;
+		}
+	}
+
+	// 装備生成   1.オブジェクト 2.弾の種類
+	void WeaponGenerator(GameObject user, int type) {
+		// 銃口
+		Vector3 muzzle;
+		// 対象のゲームオブジェクト名
+		switch (user.gameObject.name) {
+		case "Player":
+			user = GameObject.FindGameObjectWithTag("Player");
+			muzzle = new Vector3(user.transform.position.x, 0, user.transform.position.z + 1);
+			if (weaponType[type].gameObject.tag == "Blade")
+				muzzle = new Vector3(user.transform.position.x, 0, user.transform.position.z);
+			GameObject weapon = (GameObject)Instantiate(weaponType[type], muzzle, weaponType[type].transform.rotation);
+			// (Clone)対策
+			weapon.name = weaponType[type].name;
+			break;
+		}
+	}
+
+
+	// タイマー
+	protected void Timer() { timer += Time.deltaTime; }
+
+	// 経験値獲得
+	public void WeaponExp(int wexp) { exp += wexp; }
+
+	// レベルアップ
+	public void WeaponLevelUp() { level++; exp = 0; }
+
+	// 外部からのレベル参照
+	public int GetWeaponLevel() { return level; }
+}
